69de3fefd9f56013b69fd58702b980f6b6d68238
mschlkStudy <maximilian.schalk@iu-study.org>
2025-08-27 17:47:40 +0200

    Update für Coop Test

--- frontend/frontend/src/api/axios.js
+++ frontend/frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: 'http://3.120.205.182/api',
+    baseURL: 'http://3.120.205.182:8080/api',
     headers: {
         'Content-Type': 'application/json',
     },
